b408b2d8d3b16aa37e2355dad5bb14652f3056b1
Jared Reando <jared.reando@streem.pro>
2020-02-23 14:26:10 -0800

    Add csv header field validation.

--- src/pages/Admin/create_restock_report_modal.tsx
+++ src/pages/Admin/create_restock_report_modal.tsx
@@ -1,24 +1,89 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import {Header, AppText} from '../../styles/typography';
 import {FlexColumn} from "../../styles/layout";
+import {Button} from "../../styles/buttons";
+import { csvToObject } from "../../helpers/csv_to_object";
 
 const CreateRestockReportModal = () => {
+    const inputRef = useRef(null);
     const [fileName, setFileName] = useState();
+    const [fileError, setFileError] = useState<null | string>(null);
+    const [uploadData, setUploadData] = useState('no data');
+    const handleClick = (e: any) => {
+        e.preventDefault();
+        setFileError(null);
+        //@ts-ignore
+        inputRef.current!.click()
+    };
+
+    const requiredHeaders = [
+        'Storage Bin',
+        'Material',
+        'Available stock',
+        'Storage Unit',
+        'Storage Type',
+        'Storage Location',
+    ];
+
+    //checks if uploaded report contains all necessary inventory header fields
+    const hasValidHeaders = (required: string[], evaluate: string[]) => {
+        const missing: string[] = [];
+        const isValid =  required.reduce((matches, header) => {
+            if(!evaluate.includes(header)) {
+                missing.push(header);
+                matches = false;
+            }
+            return matches;
+        }, true);
+        return {isValid, missing}
+    };
+
+    const readTextFile = (file: any, callback: (data: any) => void) => {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+            const data = e.target.result;
+            const parsedData = csvToObject(data);
+            callback(parsedData)
+            const csvKeys = Object.keys(parsedData[0])
+            const isValid = hasValidHeaders(requiredHeaders, csvKeys);
+            console.log("valid? ", isValid.isValid);
+            console.log("missing: ", isValid.missing);
+        };
+        reader.readAsText(file);
+    };
+
     const handleChange = (e: any) => {
-        const fileName = e.target.files[0].name;
-        setFileName(fileName)
+        e.preventDefault();
+        const file = e.target.files[0];
+        const fileName = file.name;
+        const fileType = file.type;
+        if (fileType !== 'text/csv') {
+            setFileError('Invalid file type. Select only CSV files.');
+            return
+        }
+        setFileName(fileName);
+        readTextFile(file, setUploadData);
     };
     return (
         <FlexColumn
-            height="600px"
-            width="400px"
-            justifyContent="center"
+            height="100vh"
+            maxHeight="500px"
+            width="300px"
+            border="1px solid red"
+            backgroundColor="grey"
+            justifyContent="flex-start"
             alignItems="center"
         >
             <Header>Create Report</Header>
-            <input type='file' onChange={handleChange}/>
-            <Button></Button>
-            <AppText>{fileName}</AppText>
+            <input
+                ref={inputRef}
+                type='file' onChange={handleChange}
+                style={{display: 'none'}}
+            />
+            <Button
+                onClick={handleClick}
+            >Upload</Button>
+            <AppText>{fileError || fileName}</AppText>
         </FlexColumn>
     )
 };
