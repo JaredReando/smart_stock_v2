ec402642e323b84236a5deb1a88abe3d046e4f09
Jared Reando <jared.reando@streem.pro>
2020-08-05 13:37:17 -0700

    Prettier test run.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,17 @@
 export const SayHello = () => {
-  console.log('Hello');
+    console.log('Hello');
+};
+export const airtableFetch = async () => {
+    const response = await fetch(
+        `https://api.airtable.com/v0/appx9iHN3EuiygEw0/Returns%20(Header)`,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+            },
+        },
+    );
+    const tables = await response.json();
+    console.log('tables?', tables);
 };
--- src/pages/Client/restocking_page.styles.tsx
+++ src/pages/Client/restocking_page.styles.tsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Column, Row, Box } from "../../component_library/styles/layout";
-import {SubsectionHeader, AppText} from '../../component_library'
+import { Column, Row, Box } from '../../component_library/styles/layout';
+import { SubsectionHeader, AppText } from '../../component_library';
 
 const flexCentered = {
-  justifyContent: 'space-evenly',
-  alignItems: 'center',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
 };
 
 export const Container = styled(Column)({
-  ...flexCentered,
-  height: '100%',
-  maxHeight: '900px',
-  width: '100%',
-  maxWidth: '500px',
-  margin: '0 auto',
-  border: '1px solid green',
+    ...flexCentered,
+    height: '100%',
+    maxHeight: '900px',
+    width: '100%',
+    maxWidth: '500px',
+    margin: '0 auto',
+    border: '1px solid green',
+    touchAction: 'manipulation',
 });
 
 export const Section = styled(Row)({
-  flexGrow: 0,
-  border: '1px solid red',
-  width: '100%',
+    flexGrow: 0,
+    border: '1px solid red',
+    width: '100%',
     ...flexCentered,
 });
 
 interface DetailProps {
-  title: string;
-  data: string;
+    title: string;
+    data: string;
 }
-export const DetailRow: React.FC<DetailProps> = ({title, data}) => {
-  return (
-      <Row alignItems="center" border="1px solid red">
-        <Box mx={4}>
-          <SubsectionHeader bold>{title}</SubsectionHeader>
-        </Box>
-        <Box>
-          <SubsectionHeader >{data}</SubsectionHeader>
-        </Box>
-      </Row>
-  )
-}
+export const DetailRow: React.FC<DetailProps> = ({ title, data }) => {
+    return (
+        <Row alignItems="center" border="1px solid red">
+            <Box mx={4}>
+                <SubsectionHeader bold>{title}</SubsectionHeader>
+            </Box>
+            <Box>
+                <SubsectionHeader>{data}</SubsectionHeader>
+            </Box>
+        </Row>
+    );
+};
